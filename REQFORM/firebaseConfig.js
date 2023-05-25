4cbdf4bd1c2b0549af767ff1d64e743dd3856d76
sahaycodes <99585576+sahaycodes@users.noreply.github.com>
2023-05-25 16:48:29 +0530

    Create firebaseConfig.js

---
+++ REQFORM/firebaseConfig.js
@@ -0,0 +1,9 @@
+ projectId: "requestpage-eff62",
+  storageBucket: "requestpage-eff62.appspot.com",
+  messagingSenderId: "668905266439",
+  appId: "1:668905266439:web:fa562ade322f808994b9a2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+ export const db = getFirestore(app);
