790553f62127ad4d040ea57e50f3e27a3fba19a7
sahaycodes <99585576+sahaycodes@users.noreply.github.com>
2023-05-25 16:46:04 +0530

    Create App.js

---
+++ REQFORM/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Request from './components/request';
+
+function App() {
+  return (
+    <div className="App">
+      <Request />
+    </div>
+  );
+}
+
+export default App;
